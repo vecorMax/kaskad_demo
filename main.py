6c3d7717c45e7e0fab94b31900305a93c5e40077
vecorMax <mzhuzhgov@inbox.ru>
2019-06-30 16:17:31 +0500

    Настроена правильная работа выходов RE, DE программно (иначе выходит ошибка \xfe), получение каких-то данных

--- main.py
+++ main.py
@@ -1,49 +1,61 @@
 # Импортируем библиотеку по работе с ModbusRTU
 import struct
 import time
-
+import minimalmodbus
 # Импортируем библиотеку по работе с GPIO
 import RPi.GPIO as GPIO
-import minimalmodbus
 
 
 def kaskad():
     import serial
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(12, GPIO.OUT)
+    GPIO.setup(11, GPIO.OUT)
+    GPIO.setup(15, GPIO.OUT)
 
     serial = serial.Serial(
         port='/dev/ttyAMA0',
         baudrate=9600,
         parity=serial.PARITY_NONE,
+        bytesize=serial.EIGHTBITS,
         stopbits=serial.STOPBITS_ONE,
-        timeout=0.05,
+        timeout=0.5,
         rtscts=True
     )
 
     print('Connected:', serial.isOpen())
     print("Connected to: " + serial.portstr)
 
-    GPIO.output(12, 0)  # set high/transmit
-    # ByteStringToSend = "\x00\x01\x04\x01\x01\x00\x04\xA1\xF5"
-    # str1 = str.encode(ByteStringToSend)
-    # byt = bytes.decode(str1)
-    data = '010401050004A1F5'
+    GPIO.output(11, GPIO.LOW)  # set high/transmit
+    GPIO.output(15, GPIO.HIGH)  # set high/transmit
+    data = '0203024800010597'
     b = bytes.fromhex(data)
     print("Writing...")
     serial.write(b)
     print("Written.")
-    time.sleep(0.5)  # baud for 9600
-    GPIO.output(12, 1)  # pin set to low/receive
+    GPIO.output(11, GPIO.HIGH)  # pin set to low/receive
+    GPIO.output(15, GPIO.LOW)  # pin set to low/receive
     ReceivedData = ""
-    while (ReceivedData == ""):
-        RecievedData = serial.readline();
-        print(RecievedData.enc)
+    while ReceivedData == "":
+        RecievedData = serial.readline()
+        print(ReceivedData)
 
-    minimalmodbus.BAUDRATE = 9600
-    instrument = minimalmodbus.Instrument('/dev/ttyAMA0', 2)  # port name, slave address (in decimal)
-    temperature = instrument.read_register(262, 0, 3)  # Register number, number of decimals
-    print(temperature)
+    # minimalmodbus.BAUDRATE = 9600
+    # minimalmodbus.PARITY = 'N'
+    # minimalmodbus.BYTESIZE = 8
+    # minimalmodbus.STOPBITS = 1
+    # minimalmodbus.TIMEOUT = 0.25
+    # instrument = minimalmodbus.Instrument('/dev/ttyAMA0', slaveaddress=2,
+    #                                       mode='rtu')  # port name, slave address (in decimal)
+    # instrument.debug = True
+    # while(True):
+    #     try:
+    #         temperature = instrument.read_registers(registeraddress=257,
+    #                                                 numberOfRegisters=4, functioncode=4)  # Register number, number of decimals
+    #         print(temperature)
+    #     except IOError as e:
+    #         print("Failed to read from instrument. Error: ", str(e))
+    #     except Exception as e:\
+    #         print('Error zapros:', str(e))
 
 
 if __name__ == '__main__':
