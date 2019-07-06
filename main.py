6a23e31c5d398d9444a3f83ea5cc95b93509a455
vecorMax <mzhuzhgov@inbox.ru>
2019-07-06 18:51:48 +0500

    Налажена работа по отправке данных пробного регистра на сервер NATS для Python 3.5

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # Импортируем библиотеку по работе с ModbusRTU
-import struct
-import time
+import traceback
+import sys
 import minimalmodbus
-# Импортируем библиотеку по работе с GPIO
-import RPi.GPIO as GPIO
+import asyncio
+# Подключаем библиотечный файл для отправки сообщений.
+from smt.rpi.nats.messaging.CServiceMessaging import CServiceMessaging
 
 
-def kaskad():
+async def kaskad():
     # import serial
     # GPIO.setmode(GPIO.BOARD)
     # GPIO.setup(11, GPIO.OUT)
@@ -39,6 +40,9 @@ def kaskad():
     #     RecievedData = serial.readall()
     #     print(ReceivedData)
 
+    print("Начало работы программы.")
+    messaging = CServiceMessaging()
+
     minimalmodbus.BAUDRATE = 9600
     minimalmodbus.PARITY = 'N'
     minimalmodbus.BYTESIZE = 8
@@ -47,19 +51,45 @@ def kaskad():
     instrument = minimalmodbus.Instrument('/dev/ttyUSB0', slaveaddress=2,
                                           mode=minimalmodbus.MODE_RTU)  # port name, slave address (in decimal)
     instrument.debug = True
+
+    while 1:
+        try:
+            register_demo = instrument.read_register(545, 0)  # Номер регистра, 1 - 770 -> 77.0
+            print(register_demo)
+            await asyncio.ensure_future(messaging.send(register_demo))
+        except IOError as e:
+            print("Failed to read from instrument. Error: ", str(e))
+        except ValueError as e:
+            print('Value error:', str(e))
+        except TypeError as e:
+            print('TypeError:', str(e))
+        except Exception as e:
+            print('Exception:', str(e))
+
+
+async def main():
     try:
-        temperature = instrument.read_register(545, 0)
-        # temperature = instrument.read_registers(registeraddress=257, numberOfRegisters=1, functioncode=4)
-        print(temperature)
-    except IOError as e:
-        print("Failed to read from instrument. Error: ", str(e))
-    except ValueError as e:
-        print('Value error:', str(e))
-    except TypeError as e:
-        print('TypeError:', str(e))
+        await kaskad()
+    except KeyboardInterrupt:
+        # Выход из программы по нажатию Ctrl+C
+        print("Завершение работы Ctrl+C.")
     except Exception as e:
-        print('Exception:', str(e))
+        # Прочие исключения
+        print("Ошибка в приложении.")
+        # Подробности исключения через traceback
+        traceback.print_exc(limit=2, file=sys.stdout)
+    finally:
+        print("Сброс состояния порта в исходное.")
+        # Информируем о завершении работы программы
+        print("Программа завершена.")
 
 
 if __name__ == '__main__':
-    kaskad()
+    # asyncio.run(main())
+    ioloop = asyncio.get_event_loop()
+    tasks = [ioloop.create_task(main())]
+    wait_tasks = asyncio.wait(tasks)
+    ioloop.run_until_complete(wait_tasks)
+    ioloop.close()
+
+
